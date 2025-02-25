60d00ae8f434263448920435c776547189ec6e40
DocLivsey <doclivsey@github.com>
2025-02-25 11:40:28 +0300

    feat: trying to code shebyshev norm

--- ChebyshevsPolynoms/src/main.rs
+++ ChebyshevsPolynoms/src/main.rs
@@ -2,6 +2,10 @@ fn f(x: f64) -> f64 {
     return x.powf(3.0) * x.cos();
 }
 
+fn chebyshev(x: f64, n: i32) -> f64 {
+    return (x.acos() * n).cos();
+}
+
 fn main() {
-    println!("{}", f(4.0));
+    println!("{}", chebyshev(4.0, 3));
 }

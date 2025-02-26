ea714b134fc0cc2ba6a1ad0016bf4d773139fcef
DocLivsey <doclivsey@github.com>
2025-02-26 14:32:19 +0300

    feat: complete code from c++ to rust

--- ChebyshevsPolynoms/src/main.rs
+++ ChebyshevsPolynoms/src/main.rs
@@ -1,11 +1,19 @@
+use integrate::integrate;
+use std::f64::consts::PI;
+
 fn f(x: f64) -> f64 {
-    return x.powf(3.0) * x.cos();
+    x.powi(3) * x.cos()
 }
 
 fn chebyshev(x: f64, n: i32) -> f64 {
-    return (x.acos() * f64::from(n)).cos();
+    (n as f64 * x.acos()).cos()
 }
 
 fn main() {
-    println!("{}", chebyshev(f(4.0), 3));
+    let a = 0.0;
+    let b = PI;
+    
+    let integral = integrate(f, a, b, 1e-6);
+    
+    println!("Интеграл от f(x) на [{}, {}] ≈ {}", a, b, integral);
 }

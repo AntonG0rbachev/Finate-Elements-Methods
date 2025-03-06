8fa5dac6f0b2773e5dce74bfc3b87b7058a02a18
DocLivsey <doclivsey@github.com>
2025-03-06 15:51:50 +0300

    chore: testing plottings in vscode

--- ChebyshevsPolynoms/src/main.py
+++ ChebyshevsPolynoms/src/main.py
@@ -1,4 +1,8 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 import numpy as np
 import sympy as sm
 import scipy as sc
+
+x = np.linspace(-10, 10, 1000)
+plot.plot(x, np.cos(x))
+plot.show()
